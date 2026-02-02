5b01ad2e281d8dd955a67c061d4f15b89aae708f
EAGLE\36601 <3660192175@qq.com>
2026-02-02 21:42:07 +0800

    左侧边栏组件

--- frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,1 +1,31 @@
+import styles from "./Sidebar.module.scss";
+import { BsDatabaseFill } from "react-icons/bs";
+import { FaFolder, FaHistory } from "react-icons/fa";
 
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles["sidebar-section"]}>
+        <div className={styles["sidebar-title"]}>
+          <BsDatabaseFill />
+          数据详情
+        </div>
+        <ul className={styles["tree-view"]} id="data-tree">
+          <li className={styles["tree-item"]}>
+            <FaFolder />
+            暂无数据
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles["sidebar-section"]}>
+        <div className={styles["sidebar-title"]}>
+          <FaHistory />
+          对话历史
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
